5aa012c1c0eed121fc16af54d8a66f90f9fbe15e
ReneMarceau <rene.marceau.dev@gmail.com>
2024-02-05 23:19:33 -0500

    Some changes

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -11,11 +11,11 @@ wp config create --allow-root \
     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 wp core install --allow-root \
-    --url=$WP_URL \
+    --url=$DOMAIN_NAME \
     --title=$WP_TITLE \
-    --admin_user=$WP_USER \
-    --admin_password=$WP_PASSWORD \
-    --admin_email=$WP_EMAIL \
+    --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASSWORD \
+    --admin_email=$WP_ADMIN_EMAIL \
     --path='/var/www/wordpress'
 
 wp user create $WP_USER $WP_USER_EMAIL \
